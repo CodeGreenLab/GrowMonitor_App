e867c13e1eea208ac10525a820c702c836fe9a8f
CodeGreenLab <197512764+CodeGreenLab@users.noreply.github.com>
2025-03-23 20:11:51 -0300

    release: v2.0.1+4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "2.0.1"
     }
 
